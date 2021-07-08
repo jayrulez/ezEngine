66d6445e9d29eea44ca911c93b42a4492fb24a64
Robert Campbell <waprave@gmail.com>
2021-07-08 05:10:01 -0400

    ray tracing pipeline done

--- Code/Engine/RHIVulkan/Pipeline/VKRayTracingPipeline.cpp
+++ Code/Engine/RHIVulkan/Pipeline/VKRayTracingPipeline.cpp
@@ -33,30 +33,31 @@ VKRayTracingPipeline::VKRayTracingPipeline(VKDevice& device, const RayTracingPip
     switch (m_desc.groups[i].type)
     {
       case RayTracingShaderGroupType::kGeneral:
-        group.type = VkRayTracingShaderGroupTypeKHR::eGeneral;
+        group.type = VkRayTracingShaderGroupTypeKHR::VK_RAY_TRACING_SHADER_GROUP_TYPE_GENERAL_KHR;
         group.generalShader = get(m_desc.groups[i].general);
         break;
       case RayTracingShaderGroupType::kTrianglesHitGroup:
-        group.type = VkRayTracingShaderGroupTypeKHR::eTrianglesHitGroup;
+        group.type = VkRayTracingShaderGroupTypeKHR::VK_RAY_TRACING_SHADER_GROUP_TYPE_TRIANGLES_HIT_GROUP_KHR;
         group.closestHitShader = get(m_desc.groups[i].closest_hit);
         group.anyHitShader = get(m_desc.groups[i].any_hit);
         break;
       case RayTracingShaderGroupType::kProceduralHitGroup:
-        group.type = VkRayTracingShaderGroupTypeKHR::eProceduralHitGroup;
+        group.type = VkRayTracingShaderGroupTypeKHR::VK_RAY_TRACING_SHADER_GROUP_TYPE_PROCEDURAL_HIT_GROUP_KHR;
         group.intersectionShader = get(m_desc.groups[i].intersection);
         break;
     }
   }
 
   VkRayTracingPipelineCreateInfoKHR ray_pipeline_info{};
+  ray_pipeline_info.sType = VkStructureType::VK_STRUCTURE_TYPE_RAY_TRACING_PIPELINE_CREATE_INFO_KHR;
   ray_pipeline_info.stageCount = static_cast<uint32_t>(m_shader_stage_create_info.size());
   ray_pipeline_info.pStages = m_shader_stage_create_info.data();
   ray_pipeline_info.groupCount = static_cast<uint32_t>(groups.size());
   ray_pipeline_info.pGroups = groups.data();
   ray_pipeline_info.maxPipelineRayRecursionDepth = 1;
   ray_pipeline_info.layout = m_pipeline_layout;
-
-  m_pipeline = m_device.GetDevice().createRayTracingPipelineKHRUnique({}, {}, ray_pipeline_info).value;
+    
+   vkCreateRayTracingPipelinesKHR(m_device.GetDevice(), {}, {}, 1, &ray_pipeline_info, nullptr, &m_pipeline);
 }
 
 PipelineType VKRayTracingPipeline::GetPipelineType() const
@@ -67,6 +68,6 @@ PipelineType VKRayTracingPipeline::GetPipelineType() const
 std::vector<uint8_t> VKRayTracingPipeline::GetRayTracingShaderGroupHandles(uint32_t first_group, uint32_t group_count) const
 {
   std::vector<uint8_t> shader_handles_storage(group_count * m_device.GetShaderGroupHandleSize());
-  VkResult res = m_device.GetDevice().getRayTracingShaderGroupHandlesKHR(m_pipeline.get(), first_group, group_count, (ezUInt32)shader_handles_storage.size(), shader_handles_storage.data());
+  VkResult res = vkGetRayTracingShaderGroupHandlesKHR(m_device.GetDevice() , m_pipeline, first_group, group_count, (ezUInt32)shader_handles_storage.size(), shader_handles_storage.data());
   return shader_handles_storage;
 }
