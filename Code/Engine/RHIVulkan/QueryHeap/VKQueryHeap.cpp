f0cb4f1234af4adc48a289641d9610370dcdef2c
Robert Campbell <waprave@gmail.com>
2021-07-08 05:06:56 -0400

    Query pool done

--- Code/Engine/RHIVulkan/QueryHeap/VKQueryHeap.cpp
+++ Code/Engine/RHIVulkan/QueryHeap/VKQueryHeap.cpp
@@ -1,28 +1,29 @@
-#include <RHIVulkan/QueryHeap/VKQueryHeap.h>
 #include <RHIVulkan/Device/VKDevice.h>
+#include <RHIVulkan/QueryHeap/VKQueryHeap.h>
 
 VKQueryHeap::VKQueryHeap(VKDevice& device, QueryHeapType type, uint32_t count)
-    : m_device(device)
+  : m_device(device)
 {
-    assert(type == QueryHeapType::kAccelerationStructureCompactedSize);
-    m_query_type = VkQueryType::eAccelerationStructureCompactedSizeKHR;
-    VkQueryPoolCreateInfo desc = {};
-    desc.queryCount = count;
-    desc.queryType = m_query_type;
-    m_query_pool = m_device.GetDevice().createQueryPoolUnique(desc);
+  assert(type == QueryHeapType::kAccelerationStructureCompactedSize);
+  m_query_type = VkQueryType::VK_QUERY_TYPE_ACCELERATION_STRUCTURE_COMPACTED_SIZE_KHR;
+  VkQueryPoolCreateInfo desc = {};
+  desc.sType = VkStructureType::VK_STRUCTURE_TYPE_QUERY_POOL_CREATE_INFO;
+  desc.queryCount = count;
+  desc.queryType = m_query_type;
+  vkCreateQueryPool(m_device.GetDevice(), &desc, nullptr, &m_query_pool);
 }
 
 QueryHeapType VKQueryHeap::GetType() const
 {
-    return QueryHeapType::kAccelerationStructureCompactedSize;
+  return QueryHeapType::kAccelerationStructureCompactedSize;
 }
 
 VkQueryType VKQueryHeap::GetQueryType() const
 {
-    return m_query_type;
+  return m_query_type;
 }
 
 VkQueryPool VKQueryHeap::GetQueryPool() const
 {
-    return m_query_pool.get();
+  return m_query_pool;
 }
